92a70f3d786387a294e4fef1b16f8870e33f8883
msaryal <msaryal@bk.ru>
2023-02-16 18:41:56 +0600

    post methods

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -1,19 +1,17 @@
 <?php
 
 namespace App\Http\Controllers;
-
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Routing\Controller as BaseController;
 
-class PostController extends BaseController
+class PostController extends Controller
 {
     public function postFromGitProject()
     {
         return '';
     }
-
-    public function method2()
+    public function method()
     {
         return '';
     }
